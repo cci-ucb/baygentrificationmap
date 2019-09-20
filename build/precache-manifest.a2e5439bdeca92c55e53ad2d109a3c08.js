27b442c6570021650f976e02126cc0863d93a501
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2019-09-20 16:55:24 -0400

    added production build

---
+++ build/precache-manifest.a2e5439bdeca92c55e53ad2d109a3c08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9d4ce75c1327e102a0b66d812e7e66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "538037bb2420f253e1b7",
+    "url": "/static/css/main.e2b52d87.chunk.css"
+  },
+  {
+    "revision": "f501bb6474f1ffbd8eae",
+    "url": "/static/js/2.82e8910e.chunk.js"
+  },
+  {
+    "revision": "538037bb2420f253e1b7",
+    "url": "/static/js/main.905f3160.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
